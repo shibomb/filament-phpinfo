9b0bd188a13663d0145b3de586c9123c66eb0f63
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-08-04 22:05:36 +0900

    wip

--- resources/views/widget.blade.php
+++ resources/views/widget.blade.php
@@ -1,13 +1,14 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <x-slot name="heading>
+        <x-slot name="heading">
             {{ __('filament-phpinfo-widget::filament-phpinfo-widget.title') }}
         </x-slot>
 
-        <div x-ignore ax-load ax-load-src=" {{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-phpinfo-widget', 'shibomb/filament-phpinfo-widget') }}" x-data="filamentPhpinfoWidget()" class="text-center">
+        <div x-ignore ax-load ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-phpinfo-widget', 'shibomb/filament-phpinfo-widget') }}" x-data="filamentPhpinfoWidget()">
             <p>{{ __('filament-phpinfo-widget::filament-phpinfo-widget.description') }}</p>
             <p class="text-xl" x-text="time"></p>
-            <p>{!! $data !!}</p>
-            </div>
+        </div>
+        <div id="filament-phpinfo-widget" class="w-full">{!! $data !!}</div>
+
     </x-filament::section>
 </x-filament-widgets::widget>
