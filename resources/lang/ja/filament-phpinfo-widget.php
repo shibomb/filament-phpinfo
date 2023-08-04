44f0d6e120d0562ea4a39d62ca4e3860ce878a79
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-08-04 14:52:53 +0900

    wip

---
+++ resources/lang/ja/filament-phpinfo-widget.php
@@ -0,0 +1,7 @@
+<?php
+
+// translations for Shibomb/FilamentPhpinfoWidget
+return [
+    'title' => 'Phpinfo ウィジェット',
+    'description' => '現在時刻:',
+];
--- src/FilamentPhpinfoWidget.php
+++ src/FilamentPhpinfoWidget.php
@@ -3,8 +3,17 @@
 namespace Shibomb\FilamentPhpinfoWidget;
 
 use Filament\Widgets\Widget;
+use Illuminate\Support\Facades\Date;
 
 class FilamentPhpinfoWidget extends Widget
 {
     protected static string $view = 'filament-phpinfo-widget::widget';
+
+    protected function getViewData(): array
+    {
+        $date = new Date();
+        return [
+            'date' => $date
+        ];
+    }
 }
