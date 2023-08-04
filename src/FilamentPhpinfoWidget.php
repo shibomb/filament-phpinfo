163bec695d3a51ed521c27846c0f11c66c234b39
SHIBAHARA Hiroki <shibomb@gmail.com>
2023-08-05 00:41:43 +0900

    wip

--- src/FilamentPhpinfoWidget.php
+++ src/FilamentPhpinfoWidget.php
@@ -27,9 +27,11 @@ class FilamentPhpinfoWidget extends Widget
         $body = $dom->getElementsByTagName('body');
 
         $classes = [
-            'table' => "border-collapse w-full border border-slate-400 dark:border-slate-500 text-sm shadow-sm",
-            'th' => "w-1/3 border border-slate-300 dark:border-slate-600 font-semibold p-4 text-left",
-            'td' => "w-1/3 border border-slate-300 dark:border-slate-700 p-4"
+            'table' => 'border-collapse w-full border border-slate-400 dark:border-slate-500 text-sm shadow-sm',
+            'th' => 'w-1/3 border border-slate-300 dark:border-slate-600 font-semibold p-4 text-left break-all',
+            'td' => 'w-1/3 border border-slate-300 dark:border-slate-700 p-4 break-all',
+            'h1' => 'text-3xl pt-4',
+            'h2' => 'text-2xl pt-4'
         ];
 
         foreach ($classes as $key => $value) {
